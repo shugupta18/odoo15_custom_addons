bff1d2bfacaa1ed3feb35ecaed847479692e9f1f
SHUBHAM GUPTA <shubham.gup1811@gmail.com>
2023-12-20 19:00:08 +0530

    till 50

--- custom_addons/om_hospital/__manifest__.py
+++ custom_addons/om_hospital/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Digital Order Pvt Ltd.',
     'summary': 'Hospital management system',
     'description': """Hospital management system""",
-    'depends': ['base', 'mail'],
+    'depends': ['base', 'mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
